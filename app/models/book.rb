ec54b0b58aef8f80488cbd42dc74722a7c27210e
sansakasakananoko <misaka.seiko@gmail.com>
2024-06-18 16:37:35 +0900

    Productsにも商品画像の反映完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,10 @@
 class Book < ApplicationRecord
-    has_many :taggings, dependent: :destroy
-    has_many :tags, through: :taggings
-    has_one_attached :photo
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+  has_one_attached :photo
+
+  def picture
+    photo.variant(resize_to_limit: [300, 300])
+  end
+
 end
