b46e52268a95a07cf476ab7545c28700fce2029b
sansakasakananoko <misaka.seiko@gmail.com>
2024-06-25 16:52:13 +0900

    検索機能の追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,15 @@
 class ProductsController < ApplicationController
   before_action :move_to_signed_in
+
   def index
     @books = Book.all
     @cart = current_cart
+
+    if params[:title].present? || params[:author].present?
+      @books = @books.where("title LIKE ? AND author LIKE ?",
+                             "%#{params[:title]}%",
+                             "%#{params[:author]}%")
+    end
   end
 
   def show
@@ -10,10 +17,8 @@ class ProductsController < ApplicationController
   end
 
   private
-  def move_to_signed_in
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
-  end
 
+  def move_to_signed_in
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
